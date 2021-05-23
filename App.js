c7d2ab2d2f7df2aad6c2ad5de87ed955ee2ada86
rirohan <rirohan@vrbo.com>
2021-05-23 21:27:54 +0530

    Add all files

--- App.js
+++ App.js
@@ -1,71 +1,35 @@
-import React, { Component } from 'react';
-import {
-    StyleSheet,
-    Text,
-    View,
-    Platform
-} from 'react-native';
-import {RichTextEditor, RichTextToolbar} from './public/index';
-import KeyboardSpacer from 'react-native-keyboard-spacer';
+import * as React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import Constants from "expo-constants";
+import CodeEditor, { themes, parsers } from "./editor";
 
-export default class RichTextExample extends Component {
+const parser = parsers.python();
 
-  constructor(props) {
-    super(props);
-    this.getHTML = this.getHTML.bind(this);
-    this.setFocusHandlers = this.setFocusHandlers.bind(this);
-  }
+export default function App() {
+  const [code, setCode] = React.useState();
 
-  render() {
-    return (
-        <View style={styles.container}>
-          <RichTextEditor
-              ref={(r)=>this.richtext = r}
-              style={styles.richText}
-              initialTitleHTML={'Title!!'}
-              initialContentHTML={'Hello <b>World</b> <p>this is a new paragraph</p> <p>this is another new paragraph</p>'}
-              editorInitializedCallback={() => this.onEditorInitialized()}
-          />
-          <RichTextToolbar
-            getEditor={() => this.richtext}
-          />
-          {Platform.OS === 'ios' && <KeyboardSpacer/>}
-        </View>
-    );
-  }
-
-  onEditorInitialized() {
-    this.setFocusHandlers();
-    this.getHTML();
-  }
-
-  async getHTML() {
-    const titleHtml = await this.richtext.getTitleHtml();
-    const contentHtml = await this.richtext.getContentHtml();
-    //alert(titleHtml + ' ' + contentHtml)
-  }
-
-  setFocusHandlers() {
-    this.richtext.setTitleFocusHandler(() => {
-      //alert('title focus');
-    });
-    this.richtext.setContentFocusHandler(() => {
-      //alert('content focus');
-    });
-  }
+  return (
+    <View style={styles.container}>
+      <CodeEditor
+        code={code}
+        parser={parser}
+        theme={themes.Monokai}
+        onChangeCode={setCode}
+        style={styles.editor}
+        autofocus
+      />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: 'column',
-    backgroundColor: '#ffffff',
-    paddingTop: 40
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: "#ecf0f1",
+    alignItems: "stretch"
   },
-  richText: {
-    alignItems:'center',
-    justifyContent: 'center',
-    backgroundColor: 'transparent',
-  },
+  editor: {
+    flex: 1
+  }
 });
-
